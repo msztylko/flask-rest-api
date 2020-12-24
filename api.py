a7aec711f194b21b8e1608720c7b34826cf7d316
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-24 09:29:49 +0100

    minimal *WORKING* task list API

--- api.py
+++ api.py
@@ -1,24 +1,53 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, fields, marshal_with, reqparse, abort
 
 
 app = Flask(__name__)
 api = Api(app)
 
-tasks = {}
+TASKS = {
+    'task1': {'task': 'build an API'},
+    'task2': {'task': '???????'},
+    'task3': {'task': 'profit!'}
+}
 
+def abort_if_task_doesnt_exist(task_id):
+    if task_id not in TASKS:
+        abort(404, message=f'Task {task_id} doesnt exist')
 
-class TaskList(Resource):
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+class Task(Resource):
     def get(self, task_id):
-        return {task_id: tasks[task_id]}
+        abort_if_task_doesnt_exist(task_id)
+        return TASKS[task_id]        
+
+    def delete(self, task_id):
+        abort_if_task_doesnt_exist(task_id)
+        del TASKS[task_id]
+        return '', 204
 
     def put(self, task_id):
-        # get data from the data field of put request
-        tasks[task_id] = request.form['data']
-        return {task_id: tasks[task_id]}
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TASKS[task_id] = task
+        return task, 201
+    
+class TaskList(Resource):
+    def get(self):
+        return TASKS
 
-api.add_resource(TaskList, '/tasks/<string:task_id>')
+    def put(self, task_id):
+        args = parser.parse_args()
+        task_id = int(len(TASKS) + 1)
+        task_id = f'task_id{task_id}'
+        TASKS[task_id] = {'task': args['task']}
+        return TASKS[task_id], 201
 
+# SETUP API
+api.add_resource(Task, '/tasks/<task_id>')
+api.add_resource(TaskList, '/tasks')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
