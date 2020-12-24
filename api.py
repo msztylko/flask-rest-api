3b2c868c0d0a8e9b4c1c804b28233e91a1b9cbdb
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-24 13:28:11 +0100

    minimal task list API

--- api.py
+++ api.py
@@ -1,15 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+tasks = {}
 
-api.add_resource(HelloWorld, '/')
+
+class TaskList(Resource):
+    def get(self, task_id):
+        return {task_id: tasks[task_id]}
+
+    def put(self, task_id):
+        # get data from the data field of put request
+        tasks[task_id] = request.form['data']
+        return {task_id: tasks[task_id]}
+
+api.add_resource(TaskList, '/tasks/<string:task_id>')
 
 
 if __name__ == '__main__':
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,5 @@ import requests
 
 base_url = 'http://0.0.0.0:5000/'
 def test_api():
-    response = requests.get(base_url)
-    assert response.status_code == 200
-    
+    response = requests.put(base_url + 'tasks/0', data={'data': 'Be awesome'})
+    print(response.json()) 
