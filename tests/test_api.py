429a5178a766fb740641ce92203262837ac377a8
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-27 15:50:16 +0100

    update tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,28 @@
 import requests
+import pytest
 
 base_url = 'http://0.0.0.0:5000/'
-def test_api():
-    response = requests.put(base_url + 'tasks/0', data={'data': 'Be awesome'})
-    print(response.json()) 
+
+@pytest.mark.parametrize('task', ['be awesome', 'write great code', 'have fun', 'drink water'])
+def test_post_tasks(task):
+    response = requests.post(base_url + 'tasks', data={'task': task})
+    assert response.status_code == 201
+
+def test_get_tasks():
+    response = requests.get(base_url + 'tasks')
+    assert response.status_code == 200
+
+def test_get_task_id():
+    response = requests.get(base_url + 'tasks/3')
+    assert response.status_code == 200
+
+def test_put_task_id():
+    response = requests.put(base_url + 'tasks/1',  data={'task': 'updated task'})
+    assert response.status_code == 201
+
+def test_delete_task_id():
+    # Get ID of the last existing task to delete it
+    to_delete = requests.get(base_url + 'tasks').json()[-1]['id']
+    response = requests.delete(base_url + f'tasks/{to_delete}')
+    assert response.status_code == 240
+
